964ee8f13e8ec94b24e0bfc928bc2a04c4ba60a6
Yasin Shuman <email@yasin.io>
2017-12-06 01:25:53 -0800

    added navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -14,7 +14,7 @@ const NavBar = () => {
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Brand</a>
+          <a class="navbar-brand" href="#">IMGHOST</a>
         </div>
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
